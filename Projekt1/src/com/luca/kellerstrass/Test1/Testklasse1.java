5b55894f5c1e39add2f05b72fa2037c779e43f49
lucak <lucak@Thinkpad-13.fritz.box>
2017-06-30 00:41:24 +0200

    Hier das zweite Mal etwas verändert

--- Projekt1/src/com/luca/kellerstrass/Test1/Testklasse1.java
+++ Projekt1/src/com/luca/kellerstrass/Test1/Testklasse1.java
@@ -3,7 +3,7 @@ package com.luca.kellerstrass.Test1;
 public class Testklasse1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Das ist cool");
 
 	}
 
